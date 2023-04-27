e82b236ef1cdc5110b799f34548f83cfdc71a589
oscarostrand <oscar.ostrand@gmail.com>
2023-04-27 12:54:56 +0200

    chore: upgraded dependencies chore: set version 2.5.10

--- src/main/java/se/attini/cli/AttiniCliCommand.java
+++ src/main/java/se/attini/cli/AttiniCliCommand.java
@@ -60,7 +60,7 @@ public class AttiniCliCommand {
     public static class VersionProvider implements CommandLine.IVersionProvider {
         public final static int MAJOR = 2;
         public final static int MINOR = 5;
-        public final static int PATCH = 9;
+        public final static int PATCH = 10;
 
         public static final String VERSION_STRING = MAJOR + "." + MINOR + "." + PATCH;
 
